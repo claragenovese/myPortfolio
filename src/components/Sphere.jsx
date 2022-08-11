0a0516ef3cbdd94f4b1374e07a2a1999bda834f6
Clara Genovese <clariigenovese@gmail.com>
2022-08-11 17:20:01 -0300

    Refactor(Sphere)

--- src/components/Sphere.jsx
+++ src/components/Sphere.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-// import { Canvas } from '@react-three/fiber' 
-import { GradientTexture, Edges, Octahedron, Dodecahedron } from '@react-three/drei'
-import { Icosahedron, OrbitControls } from '@react-three/drei';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { LineBasicMaterial } from 'three';
+import { GradientTexture, Octahedron} from '@react-three/drei'
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 
 
 function Sphere() {
@@ -23,9 +21,9 @@ function Sphere() {
           <Octahedron  args={[4, 3]} smoothness={10} scale={2}>
             <meshToonMaterial wireframe>
               <GradientTexture
-                stops={[0, 1]} // As many stops as you want
-                colors={['aquamarine', 'hotpink']} // Colors need to match the number of stops
-                size={1024} // Size is optional, default = 1024
+                stops={[0, 1]} 
+                colors={['aquamarine', 'hotpink']} 
+                size={1024} 
               />
             </meshToonMaterial>
           </Octahedron>
